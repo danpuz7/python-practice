617ca66d5b9c38ee7dc2c53a4c8de463db2af3f0
Daniel Puzan <danpuz7@gmail.com>
2024-12-09 20:32:02 -0600

    added function to use with any filename

---
+++ learning/swimclub.py
@@ -0,0 +1,20 @@
+def read_swim_data(filename):
+    FOLDER = "swimdata/"
+    swimmer, age, distance, stroke = filename.removesuffix(".txt").split("-")
+    with open(FOLDER+filename) as file:
+        lines = file.readlines()
+        times = lines[0].strip().split(",")
+    converts = []
+    for t in times:
+        minutes, rest = t.split(":")
+        seconds, hundredths = rest.split(".")
+        converted_time = (int(minutes) *60*100) + (int(seconds)*100) + int(hundredths)
+        converts.append(converted_time)
+    import statistics
+    average = statistics.mean(converts)
+    mins_secs, hundredths = str(round(average / 100, 2)).split(".")
+    mins_secs = int(mins_secs)
+    minutes = mins_secs // 60
+    seconds = mins_secs - minutes*60
+    average = str(minutes) + ":" + str(seconds) + "." + hundredths
+  
