1bbd9865ab6f1dfac7aa127ed8622712fd14d6d4
Daniel Puzan <danpuz7@gmail.com>
2025-01-11 04:11:28 -0600

    finished turtlerace

--- turtleRace.py
+++ turtleRace.py
@@ -20,24 +20,48 @@ def get_number_of_racers():
         else:
             print("Please enter a number between 2 and 10!")
 
+def race(colors):
+    turtles = create_turtles(colors)
+
+    while True:
+        for racer in turtles:
+            distance = random.randrange(1, 20)
+            racer.forward(distance)
+
+            x, y = racer.pos()
+            if y >= HEIGHT // 2 - 10:
+                return colors[turtles.index(racer)]
+
+
 def create_turtles(colors):
     turtles = []
+    spacingx = WIDTH // (len(colors) + 1)
     for i, color in enumerate(colors):
         racer = turtle.Turtle()
         racer.color(color)
         racer.shape('turtle')
         racer.left(90)
+        racer.penup()
+        racer.setpos(-WIDTH//2 + (i + 1)  * spacingx, -HEIGHT//2 + 20)
+        racer.pendown()
         turtles.append(racer)
+    turtle.update()
+
+    return turtles
 
 def init_turtle():
     screen = turtle.Screen()
     screen.setup(WIDTH, HEIGHT)
     screen.title('Turtle Racers 5000')
+    return screen
 
 racers = get_number_of_racers()
 init_turtle()
+
 random.shuffle(COLORS)
 colors = COLORS[:racers]
-create_turtles(colors)
 
-
+winner = race(colors)
+print(f"The winner is the turtle with color: {winner}")
+time.sleep(5)
+turtle.bye()
